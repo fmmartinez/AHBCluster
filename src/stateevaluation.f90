8621f9b6fbcd2650ae0ba21663825320efd16129
Franz Martinez <fmmartin@ualberta.ca>
2016-12-14 01:12:00 -0700

    added evaluation of charges in complex

--- src/stateevaluation.f90
+++ src/stateevaluation.f90
@@ -4,7 +4,7 @@ use definitions
 contains
 
 subroutine get_distances_and_vectors(atoms,pairs)
-   implicit none
+implicit none
    
    integer :: i,j,n
 
@@ -35,5 +35,19 @@ subroutine get_distances_and_vectors(atoms,pairs)
       end do
    end do
 end subroutine get_distances_and_vectors
+
+subroutine update_charges_in_complex(atoms,pairs)
+implicit none
+   
+   real(8) :: rah, fr
+   type(Atom),dimension(:),intent(inout) :: atoms
+   type(AtomPairData),dimension(:,:),intent(in) :: pairs
+   
+   rah = pairs(1,3)%rij
+   fr = 0.5d0*(1d0 + (rah - 1.43d0)/sqrt((rah-1.43d0)**2+0.125d0**2))
+   atoms(1)%charge = (1d0-fr)*(-0.5d0)+fr*(-1d0)
+   atoms(2)%charge = fr*(0.5d0)
+
+end subroutine update_charges_in_complex
 
 end module stateevaluation
