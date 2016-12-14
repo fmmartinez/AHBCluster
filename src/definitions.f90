6b5ba4b916886419e4d553509783d5f048e09133
Franz Martinez <fmmartin@ualberta.ca>
2016-12-14 01:02:10 -0700

    definitions update with more parameters

--- src/definitions.f90
+++ src/definitions.f90
@@ -18,6 +18,16 @@ type ForceInAtom
    real(8),dimension(1:3) :: total
 end type ForceInAtom
 
+type Forces
+   type(ForceInAtom),dimension(:),allocatable :: inAtom
+   real(8),dimension(:,:),allocatable :: atomPair
+end type Forces
+
+real(8),parameter :: a = 11.2d0, b = 7.1d13, d = 110d0
+real(8),parameter :: na = 9.26d0, da = 0.95d0
+real(8),parameter :: nb = 11.42d0, db = 0.97d0
+real(8),parameter :: c = 0.776d0
+
 real(8),parameter :: forceToVelUnits = 418.4d0, kCoulomb = 332.06d0
 
 end module definitions
