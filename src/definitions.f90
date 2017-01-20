1b6014a621e2d589bc1d9e8758911b45671de572
Franz Martinez <fmmartin@ualberta.ca>
2017-01-20 04:51:54 -0700

    added definitions for production run and constraints

--- src/definitions.f90
+++ src/definitions.f90
@@ -31,12 +31,16 @@ type MdData
    integer :: seed, eqSteps, stepFreqEqSave, stepFreqOutTrajectory
    integer :: maxEqTries, eqPhases, eqPhaseSteps
    integer :: stepFreqVelRescale
+   integer :: prodSteps
+   integer :: nBondConstraints
    real(8) :: timeStep, halfTimeStep
    real(8) :: initialEqTempInK, targetTempInK
 end type MdData
 
 integer,parameter :: brng = VSL_BRNG_MT2203
 integer,parameter :: method = VSL_RNG_METHOD_GAUSSIAN_BOXMULLER
+integer,parameter :: maxRattleCycles = 1000
+integer,parameter :: maxShakeCycles = 10000
 
 real(8),parameter :: a = 11.2d0, b = 7.1d13, d = 110d0
 real(8),parameter :: na = 9.26d0, da = 0.95d0
@@ -48,4 +52,6 @@ real(8),parameter :: kBoltzmann = 0.831446d0
    !Boltzmann constant in amu*(A/ps)**2*K
 real(8),parameter :: KtoKcalMol = 0.00239d0
    !units of (Kcal/mol)*(ps**2/A**2)/amu
+real(8),parameter :: toleranceConstraints = 1d-7
+real(8),parameter :: constrainedRS = 1.781d0
 end module definitions
