ee155852db4b58709aa427fead9c4d7cdef87a69
Franz Martinez <fmmartin@ualberta.ca>
2017-01-22 01:28:16 -0700

    added potential energy calculations without H

--- src/energycalculation.f90
+++ src/energycalculation.f90
@@ -11,12 +11,56 @@ implicit none
    type(AtomPairData),dimension(:,:),intent(in) :: pairs
 
    rab = pairs(1,2)%rij
-   rah = pairs(1,3)%rij
-   rbh = pairs(2,3)%rij
+   rah = 1d0
+   rbh = pairs(1,2)%rij - rah
    e = b*exp(-a*rab)+d*(1-exp(-(na*(rah-da)**2)/(2d0*rah)))+c*d*(1-exp(-nb*(rbh-db)**2/(2d0*rbh)))
 end function get_complex_energy
 
+function get_complex_energy_with_H(pairs) result(e)
+implicit none
+   real(8) :: e
+   real(8) :: rab,rah,rbh
+   type(AtomPairData),dimension(:,:),intent(in) :: pairs
+
+   rab = pairs(1,2)%rij
+   rah = pairs(1,3)%rij
+   rbh = pairs(2,3)%rij
+   e = b*exp(-a*rab)+d*(1-exp(-(na*(rah-da)**2)/(2d0*rah)))+c*d*(1-exp(-nb*(rbh-db)**2/(2d0*rbh)))
+end function get_complex_energy_with_H
+
 subroutine get_complexsolvent_energy(pairs,elj,eel,e)
+implicit none
+
+   integer :: i,j,n
+   real(8),intent(out) :: elj,eel,e
+   real(8) :: r,sig,eps,qq
+   type(AtomPairData),dimension(:,:),intent(in) :: pairs
+
+   n = size(pairs,1)
+   
+   elj = 0d0   
+   do i = 1, 2
+      do j = 3, n
+         r = pairs(i,j)%rij
+         sig = pairs(i,j)%ljSig
+         eps = pairs(i,j)%ljEps
+         elj = elj + 4d0*eps*(sig**12/r**12-sig**6/r**6)
+      end do
+   end do
+   
+   eel = 0d0   
+   do i = 1, 2 
+      do j = 3, n
+         r = pairs(i,j)%rij
+         qq = pairs(i,j)%qq
+         eel = eel + kCoulomb*qq/r
+      end do
+   end do
+   
+   e = elj + eel   
+end subroutine get_complexsolvent_energy
+
+subroutine get_complexsolvent_energy_with_H(pairs,elj,eel,e)
 implicit none
 
    integer :: i,j,n
@@ -46,7 +90,7 @@ implicit none
    end do
    
    e = elj + eel   
-end subroutine get_complexsolvent_energy
+end subroutine get_complexsolvent_energy_with_H
 
 subroutine get_solventsolvent_energy(pairs,elj,eel,eb,et) 
 implicit none
