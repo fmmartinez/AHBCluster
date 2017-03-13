fea7567a5cf107103209e1359689cbd19edb82e7
Franz Martinez <fmmartin@ualberta.ca>
2017-03-13 12:53:58 -0600

    converted velocity verlet integration to form with trace in h

--- src/dynamicsroutines.f90
+++ src/dynamicsroutines.f90
@@ -606,8 +606,8 @@ implicit none
    call get_mapFactor(p)
    call get_phi_inv_r_HS_phi_matrix(p)
    call get_lambda_h_lambda_matrix(cluster,atomPairs,p)
-   call make_matrix_traceless(p%h,p%hTraceN,ht)
-   p%h = ht
+   !call make_matrix_traceless(p%h,p%hTraceN,ht)
+   !p%h = ht
 
    do i = 1, nMap
       do j = 1, nMap
